0ef6248e0c50c76b52fb83a94f8c817e5f6b273e
Milad Bazisaz <mr.miladbazisaz@gmail.com>
2017-04-10 07:55:29 +0430

    add learn how to make variable string and intiger

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 
-//this is file php this is a top
-
-
 // this is for print in php
 
 print "hello world";
 
 echo "hello worlds";
+
+// this is for variable string and intiger
+
+$hello = "hello";
+$number = 231;
